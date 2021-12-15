5cb7a3cd6a01357b0b4ab927e3a76029fb6a7c3c
KyungSik9870 <rudtlr9870@gmail.com>
2021-12-15 13:50:45 +0900

    chore : delete html file
    view 단은 사용하지 않고, api 소스만 남김

--- src/main/java/com/github/kyungsik/jwtauthentication/config/SecurityConfig.java
+++ src/main/java/com/github/kyungsik/jwtauthentication/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-			.mvcMatchers("/", "/sign-up", "/login", "/sms/verify").permitAll()
+			.mvcMatchers("/", "/sign-up", "/login", "/sms/verify", "/sms/check").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.addFilter(new JwtAuthenticationFilter(authenticationManager(), accountRepository))
